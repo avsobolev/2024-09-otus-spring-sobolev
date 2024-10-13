676bd2fa9f6f6e0cbf63a5f71fc94956df2c379b
anso <asobolev@yandex.ru>
2024-10-13 12:42:36 +0300

    [hw01] Реализовано домашнее задание

--- hw01/src/main/java/ru/otus/hw/service/TestServiceImpl.java
+++ hw01/src/main/java/ru/otus/hw/service/TestServiceImpl.java
@@ -1,16 +1,42 @@
 package ru.otus.hw.service;
 
 import lombok.RequiredArgsConstructor;
+import ru.otus.hw.dao.QuestionDao;
+import ru.otus.hw.domain.Answer;
+import ru.otus.hw.domain.Question;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 public class TestServiceImpl implements TestService {
 
     private final IOService ioService;
 
+    private final QuestionDao questionDao;
+
     @Override
     public void executeTest() {
+
         ioService.printLine("");
         ioService.printFormattedLine("Please answer the questions below%n");
         // Получить вопросы из дао и вывести их с вариантами ответов
+
+        List<Question> questions = questionDao.findAll();
+
+        for (int i = 0; i < questions.size(); i++) {
+            Question question = questions.get(i);
+            ioService.printFormattedLine("Question №%d: %s", i + 1, question.text());
+            printAnswers(question.answers());
+            ioService.printLine("");
+        }
+    }
+
+    private void printAnswers(List<Answer> answers) {
+        ioService.printLine("Aswers: ");
+        int answerCount = 0;
+        for (Answer answer : answers) {
+            answerCount++;
+            ioService.printFormattedLine("   %d. %s", answerCount, answer.text());
+        }
     }
 }
